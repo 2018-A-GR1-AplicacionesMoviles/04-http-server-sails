5f6180e4bd609a9b233e841d740d781f67b9c202
Adrian Eguez <eadepto@hotmail.com>
2018-06-09 10:56:38 -0500

    servidor pokemon

--- ServidorWeb/api/models/Pokemon.js
+++ ServidorWeb/api/models/Pokemon.js
@@ -1,5 +1,5 @@
 /**
- * Entrenador.js
+ * Pokemon.js
  *
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -8,22 +8,20 @@
 module.exports = {
 
   attributes: {
+
     nombre:{
       type:"string"
     },
-    apellido:{
+    numero:{
+      type:"number"
+    },
+    tipo:{
       type:"string"
     },
-    medallas:{
-      type:"number"
-    },
-    edad:{
-      type:"number"
-    },
-    pokemons:{
-      collection:"Pokemon",
-      via:"entrenadorId"
+    entrenadorId:{
+      model:"Entrenador"
     }
+
   },
 
 };
